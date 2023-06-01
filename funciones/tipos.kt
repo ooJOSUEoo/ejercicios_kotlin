713b8b068c708bdd32aec64a503c6b1d740221e5
Josue <sjosue5082002@gmail.com>
2023-05-31 22:50:57 -0500

    lambdas (ejercicio)

--- funciones/tipos.kt
+++ funciones/tipos.kt
@@ -7,9 +7,11 @@ fun main(){
     border("*",15)
     slogan(resultado)
     resta(num1,num2)
+    println("Valor maximo: " + max(num1,num2,25))
+    println("Promedio: " + promedio(2,5,8))
 
 }
-
+//Funciones con parametros
 fun border(c:String, l:Int){
     for (i in 1..l){
         print(c)
@@ -21,6 +23,17 @@ fun suma (a:Int, b:Int):Int{
     return a+b
 }
 
+
+//lambdas
 val slogan = {r:Any -> println(r) }
 
 val resta = {a:Int, b:Int -> println(a-b)}
+
+val max = {a:Int,b:Int,c:Int -> 
+    if (a > b && a > c) a
+    else if (b > a && b > c) b
+    else if (c > a && c > b) c
+    else "Son iguales"
+}
+
+val promedio = {n1:Int,n2:Int,n3:Int -> (n1+n2+n3)/3 }
